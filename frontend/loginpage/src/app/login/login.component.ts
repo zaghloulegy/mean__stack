76a762bd33f821733a5c2440e631403a793fe568
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-30 11:22:14 +0100

    finshing setup angular

--- frontend/loginpage/src/app/login/login.component.ts
+++ frontend/loginpage/src/app/login/login.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  id:string = '';
+  pwd:string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  loginFunc1() {
+    console.log('id->', this.id);
+    console.log('pwd->', this.pwd);
   }
-
 }
