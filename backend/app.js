437c388714a6f27c95b95e712a6508eeccb4b7e6
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-30 14:57:14 +0100

    validation is done

--- backend/app.js
+++ backend/app.js
@@ -53,3 +53,47 @@ app.post("/addUser", (req, res) => {
         res.json(responseObj);
         });
 });
+
+
+app.post("/validateUser", (req, res) => {
+    var responseObj = {};
+    var id = req.body.id;
+    var pwd = req.body.pwd;
+
+    function validateUserFunc() {
+        return new Promise((resolve, reject) => {
+          userLogin
+            .findOne({
+              id: id,
+            })
+            .then((res) => {
+              var pwdFromDB = res.pwd;
+              if (pwdFromDB == pwd) {
+                resolve();
+              } else {
+                reject("invalid password");
+              }
+            })
+            .catch((err) => {
+              console.log("error in validateUserFunc" + err);
+              reject("error in authentication");
+            });
+        });
+    }
+    function sendResponse() {
+        return new Promise((resolve, reject) => {
+            responseObj.status = "success";
+            responseObj.message = "User validated successfully";
+            res.json(responseObj);
+        })
+    }
+    validateUserFunc()
+    .then(sendResponse)
+    .catch((err) => {
+        // console.log("error in addUserFunction" + err);
+        responseObj.status = "error";
+        responseObj.message = "Error in validating user";
+        res.json(responseObj);
+        })
+        
+    })
