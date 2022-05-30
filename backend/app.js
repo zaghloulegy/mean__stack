499db5367c20c6a5ab8781f2f38e329af79ecc38
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-30 13:02:26 +0100

    adduse is done

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,55 @@
-//mongoose 
+//mongoose
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-//express js code 
-var express = require('express');
-var cors = require('cors');
+//express js code
+var express = require("express");
+var cors = require("cors");
 var app = express();
-app.set('port', (process.env.PORT || 5000));
-var bodyParser = require('body-parser');
+app.set("port", process.env.PORT || 5000);
+var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require("cors")());
-app.listen(app.get("port"))
+app.listen(app.get("port"));
 
 //Model
-var login = require('./models/login');
+var userLogin = require("./models/login");
 
-app.post("/addUser", function(req, res){
-    // console.log(req.body);
-    var user = new login({
-        id: req.body.id,
-        pwd: req.body.pwd,
+app.post("/addUser", (req, res) => {
+  var responseObj = {};
+  var id = req.body.id;
+  var pwd = req.body.pwd;
+  function addUserFunction() {
+    return new Promise((resolve, reject) => {
+      var userLoginObj = new userLogin({
+        id: id,
+        pwd: pwd,
+      });
+      userLoginObj
+        .save()
+        .then((res1) => {
+          resolve();
+        })
+        .catch((err) => {
+          console.log("error in addUserFunction" + err);
+          reject("error in register");
+        });
     });
-    user.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            console.log("User added");
-        }
+  }
+  function sendResponse() {
+    return new Promise((resolve, reject) => {
+      responseObj.status = "success";
+      responseObj.message = "User added successfully";
+      res.json(responseObj);
     });
-})
+  }
+    addUserFunction()
+    .then(sendResponse)
+    .catch((err) => {
+        // console.log("error in addUserFunction" + err);
+        responseObj.status = "error";
+        responseObj.message = "Error in adding user";
+        res.json(responseObj);
+        });
+});
